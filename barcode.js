614ea743ddafa6c8a0a13ce938a1276e00f69d89
Alin-Stefan Olaru <allenmcpotter@outlook.com>
2017-04-10 11:45:00 +0200

    Attempting to list video streams

--- barcode.js
+++ barcode.js
@@ -1,5 +1,36 @@
 var barcode = function() {
-
+	var videoSelect = document.querySelector('select#videoSource');
+	var selectors = [videoSelect];
+	function gotDevices(deviceInfos) {
+	  // Handles being called several times to update labels. Preserve values.
+	  var values = selectors.map(function(select) {
+	    return select.value;
+	  });
+	  selectors.forEach(function(select) {
+	    while (select.firstChild) {
+	      select.removeChild(select.firstChild);
+	    }
+	  });
+	  for (var i = 0; i !== deviceInfos.length; ++i) {
+	    var deviceInfo = deviceInfos[i];
+	    var option = document.createElement('option');
+	    option.value = deviceInfo.deviceId;
+	    if (deviceInfo.kind === 'videoinput') {
+	      option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+	      videoSelect.appendChild(option);
+	    } else {
+	      console.log('Some other kind of source/device: ', deviceInfo);
+	    }
+	  }
+	  selectors.forEach(function(select, selectorIndex) {
+	    if (Array.prototype.slice.call(select.childNodes).some(function(n) {
+	      return n.value === values[selectorIndex];
+	    })) {
+	      select.value = values[selectorIndex];
+	    }
+	  });
+	}
+	navigator.mediaDevices.enumerateDevices().then(gotDevices).catch()
 	var localMediaStream = null;
 	var bars = [];
 	var handler = null;
